00143d5735abe134f9a2adbf986dbbd27b5a5c4b
erinhancock794 <erinhancock794@gmail.com>
2020-11-10 18:28:35 -0700

    removed slick

--- swag_shop/js/scripts.js
+++ swag_shop/js/scripts.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
     document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("navList").classList.toggle("hide");
     document.getElementById("navList").classList.toggle("open");
 }
+
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu
