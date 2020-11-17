18542ef11d5206a05ddd13679e9dc830fb71c92b
erinhancock794 <erinhancock794@gmail.com>
2020-11-17 16:31:31 -0700

    got the api fetch to work, and added css

--- weatherapp/js/main.js
+++ weatherapp/js/main.js
@@ -1,5 +1,5 @@
 
-const apiUrl = '//api.openweathermap.org/data/2.5/weather?id=5816861&appid=880bcb631f870f8a5bd11ec2bd58f4d1&units=imperial';
+const apiUrl = "//api.openweathermap.org/data/2.5/forecast?id=5125771&appid=880bcb631f870f8a5bd11ec2bd58f4d1&units=imperial";
 
 const d = new Date();
 
@@ -11,16 +11,12 @@ const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday
 
 
 fetch (apiUrl)
-.then((response) => {
-    console.log(response)
-    response.json()
-})
+.then((response) => response.json())
 .then((info) => {
-    console.log(info);
-
     let cityName = document.createElement('h1');
     cityName.textContent = `Weather for ${info.city.name}, ${info.city.country}.`;
     document.getElementById('location').appendChild(cityName);
+
 
     for (i=0; i< info.list.length; i++) {
         let time = info.list[i].dt_txt;
@@ -34,25 +30,24 @@ fetch (apiUrl)
             dayName.className = "dayOfWeek";
             dayName.textContent = weekday[forecastDayNumber];
 
-            let dayInfo = document.createElement('div');
-            dayInfo.className = 'dayInfo';
-
             let temp = document.createElement('p');
             temp.textContent = info.list[i].main.temp + `\xB0`;
 
       
-            const iconcode = info.weather[0].icon;
-            const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+            const iconcode = info.list[i].weather[0].icon;
+            console.log(iconcode);
+            const iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+            console.log(iconPath);
             const theIcon = document.createElement('img');
-            theIcon.src = icon_path;
+            theIcon.src = iconPath;
 
             let theDay = document.createElement('div');
             theDay.className = 'theDay';
             document.getElementById('forecast').appendChild(theDay)
             theDay.appendChild(dayName);
-            theDay.appendChild(dayInfo);
             theDay.appendChild(temp);
             theDay.appendChild(theIcon);
+
 
         }
     }
